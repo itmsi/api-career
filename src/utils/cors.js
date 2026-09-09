3c8bcb6c03b10d8a4340ec2b8d81438e63616734
abdulfalaq5 <abdulfalaq5@gmail.com>
2026-09-09 13:30:20 +0700

    fix: cors

--- src/utils/cors.js
+++ src/utils/cors.js
@@ -1,23 +1,26 @@
 const whitelist = [
-  'http://localhost',
-  'https://88c98d580c697d.lhr.life'
-]
+  "http://localhost",
+  "https://88c98d580c697d.lhr.life",
+  "http://localhost:5173",
+  "https://career.motorsights.com",
+  "https://dev-career.motorsights.com",
+];
 
-let allow
-if (process.env.NODE_ENV === 'development') {
-  allow = '*'
+let allow;
+if (process.env.NODE_ENV === "development") {
+  allow = "*";
 } else {
   allow = function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error("Not allowed by CORS"));
     }
-  }
+  };
 }
 
 const corsOptions = {
-  origin: allow
-}
+  origin: allow,
+};
 
-module.exports = { corsOptions }
+module.exports = { corsOptions };
